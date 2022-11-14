c8066e5e54e05bc7bdd779e9253b11d6792479e8
kayfernander2022 <kayfernander@hotmail.com>
2022-11-13 22:45:26 -0500

    adds new item display

--- server.js
+++ server.js
@@ -18,6 +18,9 @@ res.render("index.ejs")
 
 // New Route - GET to /budgets/new 
 //- render a page with a form to create a new thing
+app.get("/budget/new", (req, res) => {
+  res.render("new.ejs");//new has the form
+});
 
 
 
@@ -25,7 +28,10 @@ res.render("index.ejs")
 
 // Create Route - POST to /budgets
 // - receive the data from the form and create a new  then redirect the user back to index
-//app.post("/budgets", (req, res) => {
+
+app.post("/budget/:id", (req, res) => {
+  
+});
 
 
 
@@ -33,6 +39,12 @@ res.render("index.ejs")
 //SHOW ROUTE - GET to /budgets/:index
 // - Returns a single 
 //Show route should ALWAYS be at the BOTTOM.
+app.get("/budget/:index", (req,res) => {
+res.render("show.ejs",
+{
+  theBudget: budget[req.params.index]
+});
+})
 
 
 const PORT = process.env.PORT || 3000 // set port to value from environment or 3000
