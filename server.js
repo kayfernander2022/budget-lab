4c85f2919419799c1006163ec8b4bcf858f79738
kayfernander2022 <kayfernander@hotmail.com>
2022-11-14 21:08:05 -0500

    working bank account added

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express'); // backend framework
 const budget = require("./models/budget.js") // import
 const app = express();
 
+
+
 //Middleware
 app.use("/static", express.static("public"))// it's going serve files from a folder called "public" under /static example public/styles.css => /static/styles.css
 
@@ -11,7 +13,21 @@ app.use(express.urlencoded({ extended: true})); // parse data from the form subm
 // INDEX ROUTE - GET to /budgets 
 //- Returns all 
 
+
 app.get("/budget", (req, res) => {
+  let bankAccount = 0;
+  budget.forEach(b => {
+    bankAccount = bankAccount + parseInt(b.amount);
+  });
+  req.app.locals.bgColor = 'clear';
+  if(bankAccount <=0)
+  {
+    req.app.locals.bgColor = 'red';
+  }
+  else if (bankAccount >= 1000){
+    req.app.locals.bgColor = 'green';
+  }
+  req.app.locals.bankAccount = bankAccount;
 res.render("index.ejs")
 });
 
@@ -20,6 +36,7 @@ res.render("index.ejs")
 // New Route - GET to /budgets/new 
 //- render a page with a form to create a new thing
 app.get("/budget/new", (req, res) => {
+
   res.render("new.ejs");
 });
 
@@ -31,7 +48,7 @@ app.get("/budget/new", (req, res) => {
 // - receive the data from the form and create a new  then redirect the user back to index
 
 app.post("/budget/", (req, res) => {
-  //console.log(req.body) //test
+ // console.log(req.body) //test
   budget.push(req.body)//push the new buget
   res.redirect("/budget")//send back to main page
 });
